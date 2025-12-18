9f78bf682ba4881b92da2ba1f7fa207a5a6135df
tadeas223 <maca.tadeas@gmail.com>
2025-12-18 14:35:58 +0100

    cors fixed

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ from flask import Flask, request, jsonify
 from data.address import Address
 import external_apis.prague_api as prague_api
 import external_apis.geograpify as geoapify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/parking", methods=["GET"])
 def post_parking():
